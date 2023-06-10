0a6376626a2949440360bc50d2d35e166891586e
Povalinna <111307356+Povalinna@users.noreply.github.com>
2023-06-10 14:11:22 +0300

    Add files via upload
    Add data.js

---
+++ data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        name: `Maria`,
+        login: `Mar25`,
+        password: `fffffff`,
+        email: `app@gmail.com`,
+        age: `25`,
+        phonNumber: `0976873344`,
+        creditCardNumber: `1234567891230099`,
+    },
+    {
+        name: `Alex`,
+        login: `Alex234`,
+        password: `fc456vbn`,
+        email: `w23@gmail.com`,
+        age: `33`,
+        phonNumber: `0953419788`,
+        creditCardNumber: `8234567891260099`,
+    },
+    {
+        name: `Anna`,
+        login: `Mirabel`,
+        password: `j000gggh`,
+        email: `v22p@gmail.com`,
+        age: `45`,
+        phonNumber: `0984516667`,
+        creditCardNumber: `1234567891230099`,
+    },
+]
